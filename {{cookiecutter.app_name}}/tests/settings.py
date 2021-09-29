1cbab12b2028734c6d173ede0ff9c57a3a0abab8
Mike Hill <mhilluniversal@gmail.com>
2021-09-29 12:31:49 -0400

    Updated CACHE_TYPE config parameter to use the new required method of full import path

--- {{cookiecutter.app_name}}/tests/settings.py
+++ {{cookiecutter.app_name}}/tests/settings.py
@@ -7,6 +7,6 @@ BCRYPT_LOG_ROUNDS = (
     4  # For faster tests; needs at least 4 to avoid "ValueError: Invalid rounds"
 )
 DEBUG_TB_ENABLED = False
-CACHE_TYPE = "simple"  # Can be "memcached", "redis", etc.
+CACHE_TYPE = "flask_caching.backends.simplecache.SimpleCache"  # full path to class
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 WTF_CSRF_ENABLED = False  # Allows form testing
